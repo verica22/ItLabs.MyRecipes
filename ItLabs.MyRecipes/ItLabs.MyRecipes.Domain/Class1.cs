478e126e95c41f30c06f8dd6da38aac3f3755eae
kostadin.kadiev@it-labs.com <kostadin.kadiev@it-labs.com>
2016-12-06 13:52:19 +0100

    Initial commit

---
+++ ItLabs.MyRecipes/ItLabs.MyRecipes.Domain/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItLabs.MyRecipes.Domain
+{
+    public class Class1
+    {
+    }
+}
